b141c0423ea237e2054f938f8e75d1a90d2ebda0
guihaopeng <email>
2021-06-22 14:07:19 +0800

    "dxsd"

---
+++ myproject/src/main/java/com/ghp/demogithub/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.ghp.demogithub.controller;
+
+import com.ghp.demogithub.entity.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: GHP
+ * @time: 2021/6/22 13:12
+ */
+@RestController
+public class UserController {
+
+    @RequestMapping("/user")
+    public Object getUser() {
+    return new User("1001","张三");
+    }
+}
